8adbc6bf7c55847f01bbc8fdd229be7825a389b8
John Blum <jblum2@apple.com>
2024-08-02 19:05:05 -0700

    Register Spring Web MVC HandlerInterceptor to intercept and analyze HTTP requests to review the JSON payload in SprinBootJsonWebApplication.

--- src/main/java/org/cp/labs/spring/boot/SpringBootJsonWebApplication.java
+++ src/main/java/org/cp/labs/spring/boot/SpringBootJsonWebApplication.java
@@ -18,39 +18,68 @@ package org.cp.labs.spring.boot;
 import static org.awaitility.Awaitility.await;
 import static org.cp.elements.lang.RuntimeExceptionsFactory.newIllegalArgumentException;
 
+import java.io.IOException;
 import java.time.Duration;
 import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 import java.util.concurrent.ThreadLocalRandom;
 
+import com.fasterxml.jackson.databind.SerializationFeature;
+
 import org.cp.domain.core.model.People;
 import org.cp.domain.core.model.Person;
+import org.cp.elements.io.IOUtils;
+import org.cp.elements.lang.ObjectUtils;
 import org.cp.elements.security.model.User;
+import org.cp.labs.model.TestUser;
+import org.cp.labs.model.serialization.json.TestUserDeserializer;
+import org.slf4j.Logger;
+import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Profile;
+import org.springframework.lang.NonNull;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
 
 /**
- * The SpringBootJsonWebApplication class...
+ * {@link SpringBootApplication} running a {@link People} and {@link User} REST API service
+ * in a Spring Web application.
  *
  * @author John Blum
+ * @see org.cp.domain.core.model.People
+ * @see org.cp.elements.security.model.User
+ * @see org.springframework.boot.SpringBootConfiguration
+ * @see org.springframework.boot.autoconfigure.SpringBootApplication
+ * @see org.springframework.boot.builder.SpringApplicationBuilder
  * @since 1.0.0
  */
+@Slf4j
 @SpringBootApplication
 @Profile(SpringBootJsonWebApplication.SPRING_APPLICATION_PROFILE)
 @SuppressWarnings("unused")
 public class SpringBootJsonWebApplication {
+
+  private static final boolean DEBUG = false;
 
   public static final String SPRING_APPLICATION_PROFILE = "json-spring-web-application";
 
@@ -63,16 +92,73 @@ public class SpringBootJsonWebApplication {
       .run(args);
   }
 
+  protected static Logger getLogger() {
+    return log;
+  }
+
+  @SpringBootConfiguration
+  @Import(TestUserDeserializer.class)
+  static class JsonWebApplicationConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(@NonNull InterceptorRegistry registry) {
+
+      if (DEBUG) {
+        registry.addInterceptor(new HandlerInterceptor() {
+
+          @Override
+          public boolean preHandle(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response,
+              @NonNull Object handler) throws IOException {
+
+            String json = new String(IOUtils.toByteArray(request.getInputStream()));
+
+            getLogger().info("JSON [{}]", json);
+            getLogger().info("Handler [{}]", ObjectUtils.getClassName(handler));
+
+            if (handler instanceof HandlerMethod handlerMethod) {
+              getLogger().info("HandlerMethod bean [{}] of type [{}] calling method [{}]",
+                handlerMethod.getBean(), handlerMethod.getBeanType().getName(), handlerMethod.getMethod().getName());
+            }
+
+            return true;
+          }
+        });
+      }
+    }
+
+    @Bean
+    Jackson2ObjectMapperBuilderCustomizer jacksonObjectMapperCustomer() {
+
+      return jacksonObjectMapperBuilder -> jacksonObjectMapperBuilder
+          .featuresToEnable(SerializationFeature.WRITE_ENUMS_USING_TO_STRING)
+          .indentOutput(true);
+    }
+  }
+
   @RestController
   @Getter(AccessLevel.PROTECTED)
   @RequestMapping("/example/rest/api/")
   static class PeopleRestApiController {
 
-    private final ConcurrentMap<String, User> userStore = new ConcurrentHashMap<>();
+    private final ConcurrentMap<String, User<UUID>> userStore = new ConcurrentHashMap<>();
 
     private final People doeFamily = doeFamily();
 
     private final ThreadLocalRandom random = ThreadLocalRandom.current();
+
+    public PeopleRestApiController() {
+
+      User<UUID> testUser = TestUser.named("testUser")
+          .withToken(UUID.randomUUID().toString())
+          .lastAccessedNow();
+
+      this.userStore.put(testUser.getName(), testUser);
+    }
+
+    @GetMapping("/people")
+    public People getPeople() {
+      return getDoeFamily();
+    }
 
     @GetMapping("/people/{id}")
     public Person getPerson(@PathVariable("id") Long id) {
@@ -86,6 +172,11 @@ public class SpringBootJsonWebApplication {
         .filter(person -> person.getId().equals(id))
         .findFirst()
         .orElseThrow(() -> newIllegalArgumentException("Person with ID [%s] not found", id));
+    }
+
+    @GetMapping("/users")
+    public Iterable<User<UUID>> getUsers() {
+      return getUserStore().values().stream().sorted().toList();
     }
 
     @GetMapping("/users/{username}")
