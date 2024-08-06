8268974395baae2381b435fe59facec6c9347d41
John Blum <jblum2@apple.com>
2024-08-05 17:04:43 -0700

    Declare @JsonPropertyOrder annotation on TestUser.

--- src/main/java/org/cp/labs/model/TestUser.java
+++ src/main/java/org/cp/labs/model/TestUser.java
@@ -24,6 +24,7 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
 import org.cp.elements.lang.CodeBlocks;
 import org.cp.elements.security.model.User;
@@ -38,12 +39,12 @@ import lombok.Setter;
  *
  * @author John Blum
  * @see org.cp.elements.security.model.User
- * @see java.time.Instant
  * @see java.util.UUID
  */
 @Getter
 @Setter(AccessLevel.PROTECTED)
 @JsonIgnoreProperties({ "new", "notNew" })
+@JsonPropertyOrder({ "name", "lastAccess", "lastAccessDateTime", "role", "token" })
 @RequiredArgsConstructor(staticName = "named")
 @SuppressWarnings("unused")
 public class TestUser implements User<UUID> {
