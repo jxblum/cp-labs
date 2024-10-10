86907ea2c7b50b55e4d42c5ea2b4d877acd15d0d
John Blum <jblum2@apple.com>
2024-10-09 20:34:27 -0700

    Create SpringBootMicrometerWebApplication to experiment with Micrometer using Spring.

---
+++ src/main/java/org/cp/labs/spring/boot/SpringBootMicrometerWebApplication.java
@@ -0,0 +1,89 @@
+/*
+ * Copyright 2017-Present Author or Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.cp.labs.spring.boot;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.ConcurrentSkipListSet;
+
+import io.micrometer.core.annotation.Counted;
+import io.micrometer.core.instrument.simple.SimpleMeterRegistry;
+
+import org.cp.elements.security.model.User;
+import org.cp.labs.model.TestUser;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+
+/**
+ * @author John Blum
+ */
+@SpringBootApplication
+@SuppressWarnings("unused")
+@Profile(SpringBootMicrometerWebApplication.SPRING_APPLICATION_PROFILE)
+public class SpringBootMicrometerWebApplication {
+
+	static final String SPRING_APPLICATION_PROFILE = "metrics";
+
+	public static void main(String[] args) {
+
+		new SpringApplicationBuilder(SpringBootMicrometerWebApplication.class)
+			.profiles(SPRING_APPLICATION_PROFILE)
+			.build()
+			.run(args);
+	}
+
+	@Bean
+	SimpleMeterRegistry simpleMeterRegistry() {
+		return new SimpleMeterRegistry();
+	}
+
+	@RestController
+	@RequestMapping("/api")
+	@Getter(AccessLevel.PROTECTED)
+	static class UserController {
+
+		private final Set<TestUser> users = new ConcurrentSkipListSet<>();
+
+		@GetMapping("/users")
+		public List<TestUser> allUsers() {
+			return getUsers().stream().sorted(Comparator.comparing(User::getName)).toList();
+		}
+
+		@Counted
+		@GetMapping("/users/{name}")
+		public TestUser getUser(@PathVariable("name") String username) {
+
+			return getUsers().stream()
+				.filter(user -> user.getName().equalsIgnoreCase(username))
+				.findFirst()
+				.orElseGet(() -> {
+					TestUser user = TestUser.named(username);
+					getUsers().add(user);
+					return user;
+				});
+		}
+	}
+}
